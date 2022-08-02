112c2d835b62b60a46fb86c0f49a072d89a32265
s0nought <70777299+s0nought@users.noreply.github.com>
2022-08-02 09:30:24 +0300

    mkrf.sh version 2

--- mkrf/mkrf.sh
+++ mkrf/mkrf.sh
@@ -5,10 +5,14 @@
 _script_name="${0##*/}"
 _script_version="2022.07.30"
 
-_file_name=$(date "+%Y.%m.%e")
-_dir_path="~/work/${_file_name//./\/}"
+_file_name=$(date "+%Y.%m.%d")
+_dir_path="work/${_file_name//.//}"
 _file_path="${_dir_path}/${_file_name}.md"
+
+pushd ~/ >/dev/null
 
 [[ -r "${_file_path}" ]] || \
     mkdir -p "${_dir_path}"
     touch "${_file_path}"
+
+popd >/dev/null
