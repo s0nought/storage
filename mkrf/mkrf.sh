cc3a8f8c7acd90543cfe43c6d77ab47ffc972b74
s0nought <70777299+s0nought@users.noreply.github.com>
2022-08-09 10:03:43 +0300

    mkrf version 3b

--- mkrf/mkrf.sh
+++ mkrf/mkrf.sh
@@ -3,7 +3,7 @@
 # make report file
 
 _script_name="${0##*/}"
-_script_version="2022.08.07"
+_script_version="2022.08.09"
 
 _file_name=$(date "+%Y.%m.%d")
 _dir_path="work/${_file_name//.//}"
@@ -12,7 +12,7 @@ _file_path="${_dir_path}/${_file_name}.md"
 pushd ~/ >/dev/null
 
 if [[ -r "${_file_path}" ]]; then
-    open "${_file_path}"
+    open ${_file_path}
 else
     mkdir -p "${_dir_path}"
     touch "${_file_path}"
